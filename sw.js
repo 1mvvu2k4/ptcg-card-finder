701ecfa21f88b4def03b41e170ab0a94568de89b
1mvvu2k4 <156487656+1mvvu2k4@users.noreply.github.com>
2026-09-24 18:05:25 +0800

    Add paired legendary stadium cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ptcg-462a3ed1ebe7';
+const CACHE_NAME = 'ptcg-7b537b6106cf';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './assets/app-icon-192.png', './assets/app-icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
