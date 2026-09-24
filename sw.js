1b5dbe6601b481ffcd18487ba0e534354d600450
1mvvu2k4 <156487656+1mvvu2k4@users.noreply.github.com>
2026-09-24 17:45:37 +0800

    Structure card skills and rules in detail view

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ptcg-6541cec68f6d';
+const CACHE_NAME = 'ptcg-462a3ed1ebe7';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './assets/app-icon-192.png', './assets/app-icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
